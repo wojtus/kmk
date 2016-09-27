621fd94305cb8d623023bdf493423bbf753375de
wojtus <wojtus@delta>
2016-09-27 21:20:14 +0200

    Controller

--- src/main/java/spectrum/kmk/Application.java
+++ src/main/java/spectrum/kmk/Application.java
@@ -1,7 +1,11 @@
 package spectrum.kmk;
 
+import java.util.List;
 import java.util.concurrent.Executor;
 
+import org.apache.commons.math3.ml.clustering.CentroidCluster;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +14,12 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import spectrum.kmk.cluster.BusinessClusterable;
 import spectrum.kmk.cluster.FixtureClusterable;
 import spectrum.kmk.cluster.TeamClusterable;
 import spectrum.kmk.service.ClusterService;
 import spectrum.kmk.service.DataImportService;
+import spectrum.kmk.source.DefaultLigaFactory;
 
 @SpringBootApplication
 @RestController
@@ -22,6 +28,8 @@ public class Application {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	private final Logger logger = LoggerFactory.getLogger(getClass());
 
 	@Autowired
 	private ClusterService<TeamClusterable> teamClusterService;
@@ -38,6 +46,19 @@ public class Application {
 		return threadPoolTaskExecutor;
 	}
 
+	@RequestMapping("/clusterFixtures")
+	String clusterFixtures() {
+		final List<CentroidCluster<BusinessClusterable<FixtureClusterable>>> objects = fixtureClusterService
+				.clusterObjects();
+		return "clustered fixtures" + objects.size();
+	}
+
+	@RequestMapping("/clusterTeams")
+	String clusterTeams() {
+		final List<CentroidCluster<BusinessClusterable<TeamClusterable>>> objects = teamClusterService.clusterObjects();
+		return "clustered teams" + objects.size();
+	}
+
 	@RequestMapping("/")
 	String home() {
 		return "Hello World";
@@ -45,7 +66,12 @@ public class Application {
 
 	@RequestMapping("/import")
 	String importData() {
-		return "Hello Docker World";
+		try {
+			importService.importData(DefaultLigaFactory.getTeams(), DefaultLigaFactory.getFixtures());
+		} catch (final Exception e) {
+			logger.error("Error importing data", e);
+		}
+		return "imported";
 	}
 
 }
